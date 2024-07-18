104fb50ffe267ff4ced6acfbf20e83948744162b
ahmedhany <elgenius10000@gmail.com>
2024-07-18 07:53:37 +0300

    update in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -16,15 +16,4 @@ export default defineConfig({
       },
     },
   },
-  // server: {
-  //   port: 3000,
-  //   open: true,
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:5000",
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ""),
-  //     },
-  //   },
-  // },
 });
