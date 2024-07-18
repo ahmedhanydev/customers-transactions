8100575421f5e39f20020119b4cb82670cca9813
ahmedhany <elgenius10000@gmail.com>
2024-07-18 07:40:11 +0300

    update in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/",
+  base: "/customers-transactions/",
   plugins: [react()],
   build: {
     outDir: "dist",
